5ed111bf30b95fb52071f86094af3ad6ece94307
marah <Marahqbj>
2023-04-09 01:51:41 +0300

    marah

--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter2/view/homecust.dart';
 import 'package:flutter2/view/init.dart';
 import 'package:flutter2/view/login.dart';
 import 'package:flutter2/view/mainpage.dart';
+import 'package:flutter2/view/selhome.dart';
 import 'package:flutter2/view/signup.dart';
 import 'package:flutter2/view/dil.dart';
 import 'package:flutter2/view/seller.dart';
@@ -46,7 +47,7 @@ class _MyAppState extends State<MyApp> {
       ],
       child: GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:MyHomePage1(),
+      home:selhome(),
        
        localizationsDelegates: [
           applocal.delegate,
---
+++ flutter2/lib/view/nav2.dart
@@ -0,0 +1,316 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter2/components/applocal.dart';
+import 'package:flutter2/utils/globalColors.dart';
+import 'package:flutter2/view/editprofile.dart';
+import 'package:flutter2/view/home.dart';
+import 'package:flutter2/view/homecust.dart';
+import 'package:flutter2/view/profile.dart';
+import 'package:flutter2/view/rest_api.dart';
+import 'package:flutter2/view/widgets/textfiled.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
+import '../main.dart';
+import '../utils/Sharedsession.dart';
+import 'about/abo2.dart';
+
+rest_api fetch=new rest_api();
+//final  date = DateTime.now();
+var rate;
+bool _lights=false;
+
+class nav2 extends StatefulWidget {
+
+
+  @override
+  State<nav2> createState() => _nav2State();
+}
+
+class _nav2State extends State<nav2> {
+  final TextEditingController niscntoraler = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var _lights;
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          ListTile(
+            leading: Icon(Icons.person,color: globalcolors.textcolor),
+            title: Text(" ${getLang(context, "pro")}",style: TextStyle(color: globalcolors.textcolor)),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const profile()),
+              )
+            },
+          ),
+          ListTile(
+            title: Text(" ${getLang(context, "contact")}",
+                style: TextStyle(color: globalcolors.textcolor)),
+            leading: Icon(Icons.call,color: globalcolors.textcolor),
+            onTap: () => {
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      AlertDialog(
+
+                        actions: [
+
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children:<Widget> [
+                                Icon(Icons.call,color: globalcolors.textcolor),
+                                SizedBox(width: 5.0),
+                          GestureDetector(
+                            child:
+
+                            Text(
+            " ${getLang(context, "call")}",
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: globalcolors.notetcolor,
+                              ),
+                            ),
+                            onTap: () {
+                              // Do something when the text is pressed
+                              _launchPhoneDialer('0592759118');
+                            },
+                          ),
+            ],
+            ),
+            ),
+
+                          const SizedBox(height: 20),
+                        ],
+
+                      )),
+            },
+          ),
+
+          SwitchListTile(
+
+            title: Text("${getLang(context, "lang")}",
+                style: TextStyle(color: globalcolors.textcolor)),
+            onChanged: (bool value) {
+              value: _lights;
+              setState(() {
+                _lights = value;
+
+              });
+
+              if(mySharedPreferences!.getString('translations')=="en"){
+                var locale= const Locale("ar","");
+                Get.updateLocale(locale);
+                _lights=true;
+                // language.savelang("ar");
+              }
+              else if (mySharedPreferences!.getString('translations')=="ar" ){
+                var  locale= Locale("en","");
+                Get.updateLocale(locale);
+                // language.savelang("en");
+                _lights=false;
+              }
+            },
+            secondary: Icon(Icons.language, color: globalcolors.textcolor), value: true,
+          ) ,
+          //////////////     /////////////////////////////////////////////////////////////
+          ListTile(
+            leading: Icon(Icons.wallet,color: globalcolors.textcolor),
+            title: Text(" ${getLang(context, "d")}",style: TextStyle(color: globalcolors.textcolor)),
+            onTap: () => {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      AlertDialog(
+
+                        actions: [
+
+                          const SizedBox(height: 20),
+
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children:<Widget> [
+                                Flexible(
+                                  child: Column(
+                                    children:<Widget> [
+
+                                      Container( child:
+                                      textfiledformat(
+                                        controller: niscntoraler,
+                                        text: "${getLang(context, "d")}",
+                                        obscure: false,
+                                        textInputType: TextInputType.text,
+                                        icon: new Icon(Icons.wallet),
+
+                                      ),
+                                      )
+                                    ],
+
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                          const SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                           ElevatedButton(
+
+                            style: ElevatedButton.styleFrom(
+
+                              shape: RoundedRectangleBorder(
+
+                                  borderRadius: BorderRadius.circular(20.0)),
+                              foregroundColor: globalcolors.maincolor,
+                              backgroundColor: globalcolors.notetcolor,
+                              minimumSize:  Size(250, 50),
+                            ),
+                            child: Text("${getLang(context, "selw")}",
+                              style: TextStyle(color: globalcolors.maincolor,fontSize: 25),
+                            ),
+                            onPressed: ()  {
+                            },
+                          ),
+            ],
+
+            ),
+                  ),
+                        ],
+
+                      )),
+            },
+          ),
+          ///////////////////////////         /////////////////////////////////////////////////////
+          ListTile(
+            leading: Icon(Icons.exit_to_app,color: globalcolors.textcolor),
+            title: Text(" ${getLang(context, "logout")}",style: TextStyle(color: globalcolors.textcolor)),
+            onTap: () => {
+
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      AlertDialog(
+
+                        actions: [
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  child: ElevatedButton(
+
+                                    style: ElevatedButton.styleFrom(
+
+                                      shape: RoundedRectangleBorder(
+
+                                          borderRadius: BorderRadius.circular(20.0)),
+                                      foregroundColor: globalcolors.maincolor,
+                                      backgroundColor: globalcolors.notetcolor,
+                                      minimumSize: Size(250, 50),
+                                    ),
+                                    child: Text("${getLang(context, "Cou")}",
+
+                                      style: TextStyle(color: globalcolors.maincolor,fontSize: 25),
+                                    ),
+                                    onPressed: ()  {
+                                      navigator?.push(MaterialPageRoute(builder: (_)=>nav2()));
+
+                                    },
+                                  ),
+                                ),
+
+
+                              ],
+
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  child: ElevatedButton(
+
+                                    style: ElevatedButton.styleFrom(
+
+                                      shape: RoundedRectangleBorder(
+
+                                          borderRadius: BorderRadius.circular(20.0)),
+                                      foregroundColor: globalcolors.maincolor,
+                                      backgroundColor: globalcolors.notetcolor,
+                                      minimumSize: const Size(250, 50),
+                                    ),
+                                    child: Text("${getLang(context, "logout")}",
+                                      style: TextStyle(color: globalcolors.maincolor,fontSize: 25),
+                                    ),
+                                    onPressed: ()  {
+                                      // navigator?.push(MaterialPageRoute(builder: (_)=>home()));
+                                      logout();
+                                    },
+                                  ),
+                                ),
+
+
+                              ],
+
+                            ),
+                          ),
+
+                          const SizedBox(height: 20),
+
+                        ],
+
+                      )
+
+              ),
+
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future feedback(rate) async {
+
+
+    final prefs = await SharedPreferences.getInstance();
+    String A = prefs.get("emailemail").toString();
+    var res=await fetch.Feedback(rate,A);
+  }
+
+
+  _launchPhoneDialer(String phoneNumber) async {
+    final url = 'tel:$phoneNumber';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+  void logout()async {
+    Sharedsession shared = new Sharedsession();
+    await shared.savename("", "");
+    await shared.savenamesuper("");
+    navigator?.push(MaterialPageRoute(builder: (_)=>home()));
+  }
+}
