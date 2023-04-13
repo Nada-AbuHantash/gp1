2a232f53aea756cb09c28fe821b8ce1881c47cf0
Nada-AbuHantash <s11821291@stu.najah.edu>
2023-04-14 02:39:10 +0300

    nana111

--- flutter2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
-import firebase_auth
-import firebase_core
 import geolocator_apple
 import location
 import path_provider_foundation
@@ -15,9 +12,6 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
